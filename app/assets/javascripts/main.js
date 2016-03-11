f6eef6c21026a1288be0f4d80f6e36078b70a461
Rita Zerrizuela <zeta@widcket.com>
2016-03-11 15:50:10 -0300

    Added error messages

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,4 +1,5 @@
 ideaCache = [];
+errors = false;
 prompt = "Me gustaría...";
 promptActive = "Me gustaría "
 
@@ -11,7 +12,9 @@ $(document).ready(function() {
 
     $("#new_idea .pure-input-3-4").val(prompt);
     $("#new_idea .pure-input-3-4").on("focus", function() {
-        $(this).val(promptActive);
+        if ($(this).val() === prompt || $("#new_idea").valid()) {
+            $(this).val(promptActive);
+        }
     });
     $("#new_idea .pure-input-3-4").blur(function() {
         if ($(this).val() === promptActive) {
@@ -24,15 +27,35 @@ $(document).ready(function() {
             "idea[texto]": {
                 required: true,
                 minlength: 20,
+                maxlength: 300,
+            }
+        },
+        messages: {
+            "idea[texto]": {
+                minlength: $.validator.format("No menos de {0} letras!"),
+                maxlength: $.validator.format("No más de {0} letras!"),
             }
         },
         showErrors: function(errorMap, errorList) {
-            // Avoid showing errors
+            noty({
+                text: errorList[0].message,
+                animation: {
+                    open: {height: 'toggle'}, // jQuery animate function property object
+                    close: {height: 'toggle'}, // jQuery animate function property object
+                    easing: 'swing', // easing
+                    speed: 500, // opening & closing animation speed
+                    killer: true,
+                    type: 'error',
+                }
+            });
+            setTimeout(function(){ $.noty.closeAll() ; }, 1500);
         },
         submitHandler: function(form) {
             $.rails.handleRemote($(form));
-        }
+        },
     });
+
+    autosize($('.pure-form .pure-input-3-4'));
 });
 
 $(window).load(function() {
