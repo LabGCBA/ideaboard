7f8f9c31220e02075a3a9e338cbfc28ca3c58c91
Rita Zerrizuela <zeta@widcket.com>
2016-03-28 11:49:34 -0300

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,31 +11,11 @@ require File.expand_path('../../app/models/direccion', __FILE__)
 require File.expand_path('../../app/models/persona', __FILE__)
 require File.expand_path('../../app/models/idea', __FILE__)
 
-subsecretarias = 2
-direcciones = 4
-ideas = 5
+Subsecretaria.create(nombre: "Anon")
 
-subsecretarias.times do |i|
-  Subsecretaria.create(nombre: "Subsecretaría #{i + 1}")
-end
+Direccion.create(nombre: "Anon", subsecretaria: Subsecretaria.find(1))
 
-direcciones.times do |i|
-  Direccion.create(nombre: "Dirección #{i + 1}", subsecretaria: Subsecretaria.find(1 + rand(subsecretarias)))
-end
-
-Persona.create(nombre: "Juan Pérez", email: "juan@perez.com", direccion: Direccion.find(1 + rand(direcciones)), password: 12345678)
-Persona.create(nombre: "Martín Fernández", email: "martin@fernandez.com", direccion: Direccion.find(1 + rand(direcciones)), password: 12345678)
-Persona.create(nombre: "Miguel Gómez", email: "miguel@gomez.com", direccion: Direccion.find(1 + rand(direcciones)), password: 12345678)
-Persona.create(nombre: "Fernando González", email: "fernando@gonzalez.com", direccion: Direccion.find(1 + rand(direcciones)), password: 12345678)
-Persona.create(nombre: "Esteban Martínez", email: "esteban@martinez.com", direccion: Direccion.find(1 + rand(direcciones)), password: 12345678)
-Persona.create(nombre: "Hannah Montana", email: "hannah@montana.com", direccion: Direccion.find(1 + rand(direcciones)), password: 12345678)
-Persona.create(nombre: "Laurel Lance", email: "laurel@lance.com", direccion: Direccion.find(1 + rand(direcciones)), password: 12345678)
-Persona.create(nombre: "Felicity Smoak", email: "felicity@smoak.com", direccion: Direccion.find(1 + rand(direcciones)), password: 12345678)
-Persona.create(nombre: "Iris West", email: "iris@west.com", direccion: Direccion.find(1 + rand(direcciones)), password: 12345678)
-Persona.create(nombre: "Barry Allen", email: "barry@allen.com", direccion: Direccion.find(1 + rand(direcciones)), password: 12345678)
-Persona.create(nombre: "Admin", email: "admin@mail.com", direccion: Direccion.find(1 + rand(direcciones)), password: 12345678, admin: true)
-
-personas = Persona.count
+Persona.create(nombre: "Anon", email: "anon@anon.com", direccion: Direccion.find(1), password: 12345678)
 
 Categoria.create(nombre: "Comida/Bebida")
 Categoria.create(nombre: "Capacitaciones")
@@ -45,16 +25,6 @@ Categoria.create(nombre: "Comunicación")
 Categoria.create(nombre: "Beneficios")
 Categoria.create(nombre: "Insumos")
 
-ideas.times do |i|
-  Idea.create(texto: "Quisiera que haya dispensers de alcohol en gel", persona: Persona.find(1 + rand(personas).round), categoria: Categoria.find(4))
-  Idea.create(texto: "Una parrilla en la terraza!", persona: Persona.find(1 + rand(personas).round), categoria: Categoria.find(4))
-  Idea.create(texto: "Me gustaría que no falte detergente en la cocina.", persona: Persona.find(1 + rand(personas).round), categoria: Categoria.find(7))
-  Idea.create(texto: "Free snacks!", persona: Persona.find(1 + rand(personas).round), categoria: Categoria.find(6))
-  Idea.create(texto: "Estaría bueno que pongan un tele en el comedor...!", persona: Persona.find(1 + rand(personas).round), categoria: Categoria.find(4))
-end
+Idea.create(texto: "Quisiera una máquina de café gratis", persona: Persona.find(1), categoria: Categoria.find(1))
 
-totalIdeas = Idea.count
 
-totalIdeas.times do |i|
-  Estado.create(texto: "Lo está viendo el Ministro.", idea: Idea.find(i + 1), persona: Persona.last)
-end
