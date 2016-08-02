4e8789bb2333e724a5b2d13606b983e002e2e5af
Rita Zerrizuela <zeta@widcket.com>
2016-08-02 09:38:12 -0300

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,59 +1,57 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-    resources :personas
-resources :categoria
-resources :comentarios
-resources :direcciones
-resources :estados
-resources :ideas
-resources :roles
-resources :subsecretarias
-resources :votes
+    namespace :admin do
+        resources :personas
+        resources :categoria
+        resources :comentarios
+        resources :direcciones
+        resources :estados
+        resources :ideas
+        resources :roles
+        resources :subsecretarias
+        resources :votes
 
-    root to: "personas#index"
-  end
+        root to: "personas#index"
+    end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
 
-  scope :expresometro do
+    root 'ideas#index'
+    get '/tv' => 'ideas#tv'
+    post '/ideas/:id' => 'ideas#vote', as: 'vote_idea'
+    post '/ideas/:id/etapa/:etapa_id' => 'ideas#update_etapa', as: 'update_etapa'
 
-      root 'ideas#index'
-      get '/tv' => 'ideas#tv'
-      post '/ideas/:id' => 'ideas#vote', as: 'vote_idea'
-      post '/ideas/:id/etapa/:etapa_id' => 'ideas#update_etapa', as: 'update_etapa' 
-      
-      # get '/baid/callback' => redirect(path: '/expresometro/personas/auth/openid_connect/callback')
-  
-      resources :direcciones, :subsecretarias, :estados, :comentarios
-  
-      resources :ideas do
-          member do
-              get :estados
-              get :comentarios
-          end
-      end
-      
-      devise_for :personas, controllers: {
-           omniauth_callbacks: 'personas/omniauth_callbacks', 
-           sessions: 'personas/sessions',
-           registrations: 'personas/registrations',
-           passwords: 'personas/passwords'
-      }
-      
-      devise_scope :persona do
-        get '/baid/login' => 'callbacks#baid_login', as: 'baid_login'
-        get '/baid/callback' => 'callbacks#baid_callback', as: 'baid_callback'
+    # get '/baid/callback' => redirect(path: '/expresometro/personas/auth/openid_connect/callback')
+
+    resources :direcciones, :subsecretarias, :estados, :comentarios
+
+    resources :ideas do
+        member do
+            get :estados
+            get :comentarios
+        end
     end
- end
+
+    devise_for :personas, controllers: {
+        omniauth_callbacks: 'personas/omniauth_callbacks',
+        sessions: 'personas/sessions',
+        registrations: 'personas/registrations',
+        passwords: 'personas/passwords'
+    }
+
+    devise_scope :persona do
+    get '/baid/login' => 'callbacks#baid_login', as: 'baid_login'
+    get '/baid/callback' => 'callbacks#baid_callback', as: 'baid_callback'
+end
+
 
   # resources :sessions, only: [:new, :create, :destroy]
   # get '/auth/:provider/callback', to: 'sessions#create'
   # get '/baid/login' => 'baid#login', as: 'baid_login'
-  
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
